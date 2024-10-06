6735b8df567c7f6c6df0f58d7ca68cc2e494e99f
Sanskar Biyani <sanskarbiyani902@gmail.com>
2024-10-06 18:57:36 +0530

    Topmost div changes, plus and equal icon added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,56 +1,55 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { BusinessInformationComponent } from './components/business-information/business-information.component';
-import { AddPartyComponent } from './components/add-party/add-party.component';
-import { AddItemComponent } from './components/add-item/add-item.component';
-import { DetailsComponent } from './components/details/details.component';
-import { PartyHomepageComponent } from './components/party-homepage/party-homepage.component';
-import { ItemHomepageComponent } from './components/item-homepage/item-homepage.component';
-import { EditDetailComponent } from './components/edit-detail/edit-detail.component';
-import { AddPartyGroupComponent } from './components/add-party-group/add-party-group.component';
-import { PaymentInoutComponent } from './components/payment-inout/payment-inout.component';
-import { LinkPaymentComponent } from './components/link-payment/link-payment.component';
-import { AutoCompleteComponent } from './components/auto-complete/auto-complete.component';
-import { PlansComponent } from './components/plans/plans.component';
-import { RegisterComponent } from './components/register/register.component';
-import { BanksComponent } from './components/banks/banks.component';
-import { BankHomepageComponent } from './components/bank-homepage/bank-homepage.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes, RouterLink } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { BusinessInformationComponent } from "./components/business-information/business-information.component";
+import { AddPartyComponent } from "./components/add-party/add-party.component";
+import { AddItemComponent } from "./components/add-item/add-item.component";
+import { DetailsComponent } from "./components/details/details.component";
+import { PartyHomepageComponent } from "./components/party-homepage/party-homepage.component";
+import { ItemHomepageComponent } from "./components/item-homepage/item-homepage.component";
+import { EditDetailComponent } from "./components/edit-detail/edit-detail.component";
+import { AddPartyGroupComponent } from "./components/add-party-group/add-party-group.component";
+import { PaymentInoutComponent } from "./components/payment-inout/payment-inout.component";
+import { LinkPaymentComponent } from "./components/link-payment/link-payment.component";
+import { AutoCompleteComponent } from "./components/auto-complete/auto-complete.component";
+import { PlansComponent } from "./components/plans/plans.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { BanksComponent } from "./components/banks/banks.component";
+import { BankHomepageComponent } from "./components/bank-homepage/bank-homepage.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'sidebar', component: SidebarComponent },
-  { path: 'businessinfo', component: BusinessInformationComponent },
-  { path: 'addparty', component: AddPartyComponent },
-  { path: 'additem', component: AddItemComponent },
-  { path: 'addPartyGroup', component: AddPartyGroupComponent },
-  { path: 'banks', component: BanksComponent},
-  { path: 'banks-homepage', component: BankHomepageComponent},
-  { path: 'dashboard', component: DashboardComponent},
-  { path: 'inout', component: LinkPaymentComponent},
+  { path: "", component: LoginComponent },
+  { path: "login", component: LoginComponent },
+  { path: "sidebar", component: SidebarComponent },
+  { path: "businessinfo", component: BusinessInformationComponent },
+  { path: "addparty", component: AddPartyComponent },
+  { path: "additem", component: AddItemComponent },
+  { path: "addPartyGroup", component: AddPartyGroupComponent },
+  { path: "banks", component: BanksComponent },
+  { path: "banks-homepage", component: BankHomepageComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "inout", component: LinkPaymentComponent },
 
-  { path: 'party-homepage', component: PartyHomepageComponent },
-  { path: 'item-homepage', component: ItemHomepageComponent },
-  { path: 'pin', component: PaymentInoutComponent },
-  { path: 'Payment-Link', component: LinkPaymentComponent },
-  { path: 'plans', component: PlansComponent },
-  { path: 'Register', component: RegisterComponent },
-  { path: 'linked', component: LinkPaymentComponent },
+  { path: "party-homepage", component: PartyHomepageComponent },
+  { path: "item-homepage", component: ItemHomepageComponent },
+  { path: "pin", component: PaymentInoutComponent },
+  { path: "Payment-Link", component: LinkPaymentComponent },
+  { path: "plans", component: PlansComponent },
+  { path: "Register", component: RegisterComponent },
+  { path: "linked", component: LinkPaymentComponent },
 
-  { path: 'Sale', pathMatch: 'full', redirectTo: '/Sale-Invoice' },
-  { path: 'Purchase', pathMatch: 'full', redirectTo: '/Purchase-Bills' },
+  { path: "Sale", pathMatch: "full", redirectTo: "/Sale-Invoice" },
+  { path: "Purchase", pathMatch: "full", redirectTo: "/Purchase-Bills" },
   // All other paths above this line.
-  { path: ':type', component: DetailsComponent},
-  { path: ':type/:fnType', component: EditDetailComponent},
-  { path: ':type/:fnType/:invoiceNo', component: EditDetailComponent},
-
+  { path: ":type", component: DetailsComponent },
+  { path: ":type/:fnType", component: EditDetailComponent },
+  { path: ":type/:fnType/:invoiceNo", component: EditDetailComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
